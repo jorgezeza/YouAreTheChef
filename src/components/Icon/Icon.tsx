fe708aefa8aaee5176b76ff02193d8210f728cb2
jorgezeza <jorgezeza2010@gmail.com>
2025-05-19 21:23:38 -0300

    create Icon Component

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {EyeOnIcon} from '../../assests/icons/EyeOnIcon'
+import {EyeOffIcon} from '../../assests/icons/EyeOffIcon'
+import {themeColors} from '../../theme/theme'
+import {useAppTheme} from '../../hooks/useAppTheme'
+
+export interface IconBase {
+  size?: number
+  color?: string
+}
+
+interface Props {
+  name: IconName
+  color?: themeColors
+  size?: number
+}
+
+export function Icon({name, color = 'backgroundContrast', size}: Props) {
+  const {colors} = useAppTheme()
+  const SVGIcon = iconRegistry[name]
+  return <SVGIcon color={colors[color]} size={size} />
+}
+
+const iconRegistry = {
+  eyeOn: EyeOnIcon,
+  eyeOff: EyeOffIcon
+}
+
+type IconType = typeof iconRegistry
+
+type IconName = keyof IconType
