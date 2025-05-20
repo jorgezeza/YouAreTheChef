fda844d70d9dcf05887bf74489eedd2a0edf59e6
jorgezeza <jorgezeza2010@gmail.com>
2025-05-19 21:31:32 -0300

    now using the useAppTheme hook

--- src/components/ActivityIndicator/ActivityIndicator.tsx
+++ src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
-import { ActivityIndicatorProps, ActivityIndicator as RNActivityIndicator } from 'react-native'
-import { Theme, themeColors } from '../../theme/theme'
-import { useTheme } from '@shopify/restyle'
+import {
+  ActivityIndicatorProps,
+  ActivityIndicator as RNActivityIndicator
+} from 'react-native'
+import {themeColors} from '../../theme/theme'
+import {useAppTheme} from '../../hooks/useAppTheme'
 
 interface Props extends Omit<ActivityIndicatorProps, 'color'> {
   color: themeColors
 }
 
-export function ActivityIndicator({ color }: Props) {
-  const {colors} = useTheme<Theme>()
+export function ActivityIndicator({color}: Props) {
+  const {colors} = useAppTheme()
   return <RNActivityIndicator color={colors[color]} />
 }
