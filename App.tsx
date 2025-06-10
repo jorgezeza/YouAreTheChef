37e91e8a6d93710d6518d04fb279199dbae1c735
jorgezeza <jorgezeza2010@gmail.com>
2025-06-10 19:23:15 -0300

    removed LoginScreen from App

--- App.tsx
+++ App.tsx
@@ -1,69 +1,19 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react'
-import {Image, ImageBackground, SafeAreaView, View} from 'react-native'
 import {ThemeProvider} from '@shopify/restyle'
 import {theme} from './src/theme/theme'
-import {Box, BoxProps} from './src/components/Box/Box'
-import {Button} from './src/components/Button/Button'
-import {Text} from './src/components/Text/Text'
-import {TextInput} from './src/components/TextInput/TextInput'
-import {Icon} from './src/components/Icon/Icon'
+import {BoxProps} from './src/components/Box/Box'
+import {LoginScreen} from './src/screens/auth/LoginScreen'
+import {SafeAreaProvider} from 'react-native-safe-area-context'
 
 function App(): React.JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaView style={{flex: 1}}>
-        <ImageBackground
-          source={require('./src/assests/images/backgroundLogin.png')}
-          resizeMode="cover"
-          style={{width: '100%', height: '100%'}}>
-          <View
-            style={{
-              paddingVertical: 24,
-              paddingHorizontal: 24
-            }}>
-            <Box {...$containerPersonLogin}>
-              <Image source={require('./src/assests/images/personLogin.png')} />
-            </Box>
-            <Box>
-              <Box>
-                <TextInput
-                  errorMessage="Mensagem de Error"
-                  label="E-mail"
-                  placeholder="Digite seu e-mail"
-                  LeftComponent={<Icon color="redPrimary" name="email"/>}
-                  boxProps={{mb: 's20'}}
-                />
-              </Box>
-              <Box>
-                <TextInput
-                  label="Senha"
-                  placeholder="Digite sua senha"
-                  LeftComponent={<Icon name="lock" />}
-                  RightComponent={<Icon name="eyeOff" />}
-                  boxProps={{mb: 's20'}}
-                />
-              </Box>
-            </Box>
-
-            <Box>
-              <Button title="Entrar" />
-              <Text color="primaryContrast" mb="s8" mt="s8" textAlign="center">
-                Esqueci minha senha
-              </Text>
-              <Button preset="secondary" title="Crie sua conta" />
-            </Box>
-          </View>
-        </ImageBackground>
-      </SafeAreaView>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <LoginScreen />
+      </ThemeProvider>
+    </SafeAreaProvider>
   )
-}
-
-const $containerPersonLogin: BoxProps = {
-  alignItems: 'center',
-  mt: 's56',
-  mb: 's56'
 }
 
 export default App
