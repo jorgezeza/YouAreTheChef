d86b02d2e7729652961a45813a68f9867026075f
jorgezeza <jorgezeza2010@gmail.com>
2025-06-11 21:36:46 -0300

    feat: canGoBack, scrollable and imageBackground

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,18 +1,64 @@
 import React from 'react'
 import {Box} from '../Box/Box'
-import {useAppTheme} from '../../hooks/useAppTheme'
 import {useAppSafeArea} from '../../hooks/useAppSafeArea'
+import {Icon} from '../Icon/Icon'
+import {Text} from '../Text/Text'
+import {
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native'
+import {ScrollViewContainer, ViewContainer} from './components/ScreenContainer'
+import {useAppTheme} from '../../hooks/useAppTheme'
 
 interface ScreenProps {
   children: React.ReactNode
+  canGoBack?: boolean
+  scrollable?: boolean
+  imageBackground: ImageNames
 }
 
-export function Screen({children}: ScreenProps) {
-  const {top} = useAppSafeArea()
-  const {spacing} = useAppTheme()
+export function Screen({
+  children,
+  canGoBack = false,
+  scrollable = false,
+  imageBackground
+}: ScreenProps) {
+  const {top, bottom} = useAppSafeArea()
+  const {colors} = useAppTheme()
+  const image = imageBackgroundRegistry[imageBackground] || ''
+  const Container = scrollable ? ScrollViewContainer : ViewContainer
   return (
-    <Box paddingBottom="s24" paddingHorizontal="s24" style={{paddingTop: top}}>
-      {children}
-    </Box>
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <ImageBackground
+        source={image}
+        style={{flex: 1}}>
+        <Container>
+          <Box
+            paddingBottom="s24"
+            paddingHorizontal="s24"
+            style={{paddingTop: top, paddingBottom: bottom}}>
+            {canGoBack && (
+              <Box mb="s24" flexDirection="row">
+                <Icon name="lock" color="primary" />
+                <Text preset="mediumFont" marginLeft="s8">
+                  Voltar
+                </Text>
+              </Box>
+            )}
+            {children}
+          </Box>
+        </Container>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   )
 }
+
+const imageBackgroundRegistry = {
+  backgroundLogin: require('../../assests/images/backgroundLogin.png'),
+}
+
+type ImageType = typeof imageBackgroundRegistry
+type ImageNames = keyof ImageType
