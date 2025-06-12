2c550e9ec748e287ab4e6d63d41b3123c170c15c
jorgezeza <jorgezeza2010@gmail.com>
2025-06-11 21:34:30 -0300

    feat: add onPress event and hitSlop

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -3,9 +3,10 @@ import {EyeOnIcon} from '../../assests/icons/EyeOnIcon'
 import {EyeOffIcon} from '../../assests/icons/EyeOffIcon'
 import {themeColors} from '../../theme/theme'
 import {useAppTheme} from '../../hooks/useAppTheme'
-import { UserIcon } from '../../assests/icons/UserIcon'
-import { EmailIcon } from '../../assests/icons/EmailIcon'
-import { LockIcon } from '../../assests/icons/LockIcon'
+import {UserIcon} from '../../assests/icons/UserIcon'
+import {EmailIcon} from '../../assests/icons/EmailIcon'
+import {LockIcon} from '../../assests/icons/LockIcon'
+import {Pressable} from 'react-native'
 
 export interface IconBase {
   size?: number
@@ -16,11 +17,26 @@ interface Props {
   name: IconName
   color?: themeColors
   size?: number
+  onPress?: () => void
 }
 
-export function Icon({name, color = 'backgroundContrast', size}: Props) {
+export function Icon({
+  name,
+  color = 'backgroundContrast',
+  size,
+  onPress
+}: Props) {
   const {colors} = useAppTheme()
   const SVGIcon = iconRegistry[name]
+
+  if (onPress) {
+    return (
+      <Pressable hitSlop={10} onPress={onPress}>
+        <SVGIcon color={colors[color]} size={size} />
+      </Pressable>
+    )
+  }
+
   return <SVGIcon color={colors[color]} size={size} />
 }
 
