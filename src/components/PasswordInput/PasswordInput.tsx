e3fe21348cf76a9ddc438f60740d2cccf2a24fc9
jorgezeza <jorgezeza2010@gmail.com>
2025-06-11 21:25:48 -0300

    create PasswordInput Component

---
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+import {TextInput, TextInputProps} from '../TextInput/TextInput'
+import {Icon} from '../Icon/Icon'
+import {Pressable} from 'react-native'
+
+type PasswordInputProps = Omit<TextInputProps, 'RightComponent'>
+
+export function PasswordInput(props: TextInputProps) {
+  const [isSecureTextEntry, setSecureTextEntry] = useState(true)
+
+  function toggleSecureTextEntry() {
+    setSecureTextEntry(prev => !prev)
+  }
+  return (
+    <TextInput
+      secureTextEntry={isSecureTextEntry}
+      {...props}
+      RightComponent={
+        <Icon
+          onPress={toggleSecureTextEntry}
+          name={isSecureTextEntry ? 'eyeOn' : 'eyeOff'}
+        />
+      }
+    />
+  )
+}
