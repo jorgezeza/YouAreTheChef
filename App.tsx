07a70e0fdeff125d50178160cde90cb0f0ab3d8a
jorgezeza <jorgezeza2010@gmail.com>
2025-06-16 21:14:20 -0300

    work in production

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react'
 import {ThemeProvider} from '@shopify/restyle'
 import {theme} from './src/theme/theme'
-import {BoxProps} from './src/components/Box/Box'
-import {LoginScreen} from './src/screens/auth/LoginScreen'
 import {SafeAreaProvider} from 'react-native-safe-area-context'
+import { Router } from './src/routes/Routes'
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <LoginScreen />
+        <Router />
       </ThemeProvider>
     </SafeAreaProvider>
   )
--- src/components/PasswordInput/PasswordInput.tsx
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -1,9 +1,6 @@
 import React, {useState} from 'react'
 import {TextInput, TextInputProps} from '../TextInput/TextInput'
 import {Icon} from '../Icon/Icon'
-import {Pressable} from 'react-native'
-
-type PasswordInputProps = Omit<TextInputProps, 'RightComponent'>
 
 export function PasswordInput(props: TextInputProps) {
   const [isSecureTextEntry, setSecureTextEntry] = useState(true)
