abb318f308300079b1760a55bcef3c617a375ba4
jorgezeza <jorgezeza2010@gmail.com>
2025-06-25 00:10:38 -0300

    install react-hook-form and react hook form controlled inputs

--- src/screens/auth/LoginScreen/LoginScreen.tsx
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Controller, useForm} from 'react-hook-form'
 import {NativeStackScreenProps} from '@react-navigation/native-stack'
 import {Box, BoxProps} from '../../../components/Box/Box'
 import {TextInput} from '../../../components/TextInput/TextInput'
@@ -7,12 +8,25 @@ import {Button} from '../../../components/Button/Button'
 import {Text} from '../../../components/Text/Text'
 import {Screen} from '../../../components/Screen/Screen'
 import {PasswordInput} from '../../../components/PasswordInput/PasswordInput'
-import {Image} from 'react-native'
+import {Alert, Image} from 'react-native'
 import {RootStackParamList} from '../../../routes/Routes'
 
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'LoginScreen'>
 
+type LoginFormType = {
+  email: string
+  password: string
+}
+
 export function LoginScreen({navigation}: ScreenProps) {
+  const {control, formState, handleSubmit} = useForm<LoginFormType>({
+    defaultValues: {
+      email: '',
+      password: ''
+    },
+    mode: 'onChange'
+  })
+
   function navigateToSignUpScreen() {
     navigation.navigate('SignUpScreen')
   }
@@ -20,34 +34,66 @@ export function LoginScreen({navigation}: ScreenProps) {
   const navigateToForgotPasswordScreen = () => {
     navigation.navigate('ForgotPasswordScreen')
   }
+
+  const submitForm = ({email, password}: LoginFormType) =>
+    Alert.alert(`Email: ${email}, Senha: ${password}`)
   return (
-    <Screen imageBackground="backgroundLogin">
+    <Screen imageBackground="backgroundLogin" scrollable>
       <Box {...$containerPersonLogin}>
         <Image source={require('../../../assests/images/personLogin.png')} />
       </Box>
       <Box>
-        <Box>
-          <TextInput
-            errorMessage="Mensagem de Error"
-            label="E-mail"
-            placeholder="Digite seu e-mail"
-            LeftComponent={<Icon color="redPrimary" name="email" />}
-            boxProps={{mb: 's20'}}
-          />
-        </Box>
-        <Box>
-          <PasswordInput
-            label="Senha"
-            placeholder="Digite sua senha"
-            LeftComponent={<Icon name="lock" />}
-            RightComponent={<Icon name="eyeOff" />}
-            boxProps={{mb: 's20'}}
-          />
-        </Box>
+        <Controller
+          control={control}
+          name="email"
+          rules={{
+            required: 'E-mail obrigatório',
+            pattern: {
+              value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+              message: 'E-mail inválido'
+            }
+          }}
+          render={({field, fieldState}) => (
+            <TextInput
+              errorMessage={fieldState.error?.message}
+              value={field.value}
+              onChangeText={field.onChange}
+              label="E-mail"
+              placeholder="Digite seu e-mail"
+              LeftComponent={<Icon color="redPrimary" name="email" />}
+              boxProps={{mb: 's20'}}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="password"
+          rules={{
+            required: 'Senha obrigatória',
+            minLength: {
+              value: 3,
+              message: 'Senha deve ter no mínimo 3 caracteres'
+            }
+          }}
+          render={({field, fieldState}) => (
+            <PasswordInput
+              errorMessage={fieldState.error?.message}
+              label="Senha"
+              value={field.value}
+              onChangeText={field.onChange}
+              placeholder="Digite sua senha"
+              LeftComponent={<Icon name="lock" />}
+              RightComponent={<Icon name="eyeOff" />}
+              boxProps={{mb: 's20'}}
+            />
+          )}
+        />
       </Box>
 
       <Box>
-        <Button title="Entrar" />
+        <Button title="Entrar" onPress={handleSubmit(submitForm)} />
+
         <Text
           onPress={navigateToForgotPasswordScreen}
           color="primaryContrast"
@@ -56,6 +102,7 @@ export function LoginScreen({navigation}: ScreenProps) {
           textAlign="center">
           Esqueci minha senha
         </Text>
+
         <Button
           onPress={navigateToSignUpScreen}
           preset="secondary"
