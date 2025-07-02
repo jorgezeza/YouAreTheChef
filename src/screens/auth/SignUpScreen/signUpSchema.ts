11dc95067874484937ab3f6cf012c1fdb0a85759
jorgezeza <jorgezeza2010@gmail.com>
2025-07-02 20:28:43 -0300

    create signUpSchema

---
+++ src/screens/auth/SignUpScreen/signUpSchema.ts
@@ -0,0 +1,13 @@
+import {z} from 'zod'
+
+
+const userNameRegex = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/gim;
+
+export const signUpSchema = z.object({
+  username: z.string().regex(userNameRegex, 'username inválido').toLowerCase(),
+  email: z.string().email('email inválido'),
+  password: z.string().min(3, 'Senha deve ter no mínumo 3 caracteres'),
+  confirmPassword: z.string().min(3, 'Senha deve ter no mínumo 3 caracteres')
+})
+
+export type SignUpSchema = z.infer<typeof signUpSchema>
