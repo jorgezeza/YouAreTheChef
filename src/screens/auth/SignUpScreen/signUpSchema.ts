d3e9d545556a992dbfde889e6cb32b1e7277b39d
jorgezeza <jorgezeza2010@gmail.com>
2025-07-03 19:45:01 -0300

    refine method added, to compare password field values

--- src/screens/auth/SignUpScreen/signUpSchema.ts
+++ src/screens/auth/SignUpScreen/signUpSchema.ts
@@ -1,4 +1,4 @@
-import {z} from 'zod'
+import { z } from 'zod'
 
 
 const userNameRegex = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/gim;
@@ -8,6 +8,9 @@ export const signUpSchema = z.object({
   email: z.string().email('email inválido'),
   password: z.string().min(3, 'Senha deve ter no mínumo 3 caracteres'),
   confirmPassword: z.string().min(3, 'Senha deve ter no mínumo 3 caracteres')
+}).refine((value) => value.password === value.confirmPassword, {
+  message: 'As senhas não coincidem',
+  path: ['confirmPassword']
 })
 
 export type SignUpSchema = z.infer<typeof signUpSchema>
