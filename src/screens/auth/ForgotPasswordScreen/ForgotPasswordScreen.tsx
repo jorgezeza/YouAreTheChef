51cb2aabcedccb4ac7e2df557e3dffb1dd29a9c9
jorgezeza <jorgezeza2010@gmail.com>
2025-07-08 19:58:19 -0300

    feat: ForgotPasswordScreen, form controled by react-hook-form with zod

--- src/screens/auth/ForgotPasswordScreen/ForgotPasswordScreen.tsx
+++ src/screens/auth/ForgotPasswordScreen/ForgotPasswordScreen.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import {Screen} from '../../../components/Screen/Screen'
 import {Text} from '../../../components/Text/Text'
-import {TextInput} from '../../../components/TextInput/TextInput'
 import {Icon} from '../../../components/Icon/Icon'
 import {Button} from '../../../components/Button/Button'
 import {NativeStackScreenProps} from '@react-navigation/native-stack'
 import {RootStackParamList} from '../../../routes/Routes'
 import {useResetNavigationSuccess} from '../../../hooks/useResetNavigationSuccess'
+import {useForm} from 'react-hook-form'
+import {
+  forgotPasswordSchema,
+  ForgotPasswordSchema
+} from './forgotPasswordSchema'
+import {FormTextInput} from '../../../components/Form/FormTextInput'
+import {zodResolver} from '@hookform/resolvers/zod'
 
 type ScreenProps = NativeStackScreenProps<
   RootStackParamList,
@@ -14,6 +20,13 @@ type ScreenProps = NativeStackScreenProps<
 >
 
 export function ForgotPasswordScreen({navigation}: ScreenProps) {
+  const {control, formState, handleSubmit} = useForm<ForgotPasswordSchema>({
+    resolver: zodResolver(forgotPasswordSchema),
+    defaultValues: {
+      email: ''
+    },
+    mode: 'onChange'
+  })
   const {reset} = useResetNavigationSuccess()
   const submitForm = () => {
     reset({
@@ -30,14 +43,19 @@ export function ForgotPasswordScreen({navigation}: ScreenProps) {
       <Text preset="mediumFont" mb="s24">
         Digite seu e-mail e enviaremos as instruções para redefinição de senha
       </Text>
-      <TextInput
-        errorMessage="Mensagem de Error"
+      <FormTextInput
+        control={control}
+        name="email"
         label="E-mail"
         placeholder="Digite seu e-mail"
         LeftComponent={<Icon color="redPrimary" name="email" />}
         boxProps={{mb: 's20'}}
       />
-      <Button onPress={submitForm} title="Recuperar senha" />
+      <Button
+        disabled={!formState.isValid}
+        onPress={handleSubmit(submitForm)}
+        title="Recuperar senha"
+      />
     </Screen>
   )
 }
