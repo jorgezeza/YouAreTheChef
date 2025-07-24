1086dd92fc7ca72fd8e1b440e63e681c0cde0b49
jorgezeza <jorgezeza2010@gmail.com>
2025-07-24 20:21:00 -0300

    caution, eslint version and settings have changed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,41 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  rules: {
-    semi: ['error', 'never'],
-    'comma-dangle': ['error', 'never']
-  }
-}
+  plugins: ['import'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'import/order': [
+          'error',
+          {
+            groups: ['external', 'builtin', 'internal', 'parent', 'sibling'],
+            pathGroups: [
+              {
+                pattern: 'react+(|-native)',
+                group: 'external',
+                position: 'before',
+              },
+              {
+                pattern: '@+(routes|screens|components|hooks|theme)',
+                group: 'internal',
+                position: 'before',
+              },
+              {
+                pattern: './',
+                group: 'internal',
+                position: 'before',
+              },
+            ],
+            pathGroupsExcludedImportTypes: ['react+(|-native)'],
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+            'newlines-between': 'always',
+          },
+        ],
+      },
+    },
+  ],
+};
